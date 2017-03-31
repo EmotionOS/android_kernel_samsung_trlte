17046baf4f6694a329a0ef63344f16003dac43f2
underscoremone <underscoremone@gmail.com>
2017-03-31 16:59:54 +0100

    Updates from N910FXXS1DQC3
    samsung's SM-N910F_EUR_MM_Opensource (N910FXXS1DQC3)
    http://opensource.samsung.com/reception/receptionSub.do?method=sub&sub=F&searchValue=n910f
    
    Change-Id: I362588a55bcf1ee2448bb8fe96809bd9768a7405

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -689,7 +689,7 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 
-	pr_info("%s: ctrl=%p ndx=%d\n", __func__, ctrl, ctrl->ndx);
+	pr_info("%s: ctrl=%pK ndx=%d\n", __func__, ctrl, ctrl->ndx);
 
 #if defined(CONFIG_FB_MSM_MDSS_SAMSUNG)
 	if (ctrl->ndx == DSI_CTRL_0)
@@ -768,7 +768,7 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 
-	pr_info("%s: ctrl=%p ndx=%d\n", __func__, ctrl, ctrl->ndx);
+	pr_info("%s: ctrl=%pK ndx=%d\n", __func__, ctrl, ctrl->ndx);
 
 #if defined(CONFIG_FB_MSM_MDSS_SAMSUNG)
 	if (pinfo->esd_check_enabled) {
@@ -825,7 +825,7 @@ static int mdss_dsi_panel_low_power_config(struct mdss_panel_data *pdata,
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 
-	pr_debug("%s: ctrl=%p ndx=%d enable=%d\n", __func__, ctrl, ctrl->ndx,
+	pr_debug("%s: ctrl=%pK ndx=%d enable=%d\n", __func__, ctrl, ctrl->ndx,
 		enable);
 
 	/* Any panel specific low power commands/config */
